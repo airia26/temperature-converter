878667b98f620c7fdc05f52f9f31683e8464da5c
Ria <singhalria27@gmail.com>
2026-06-12 14:43:55 +0530

    Add files via upload

---
+++ avg_numbers.c
@@ -0,0 +1,24 @@
+// Calculation of Average Numbers
+#include <stdio.h>
+
+#define N 10
+
+int main()
+{
+    int count ;
+    float sum, average, number;
+    sum = 0;
+    count = 0;
+    while (count<N)
+    {
+        printf("Enter number %d:", count + 1);
+        scanf("%f", &number);
+        sum = sum + number;
+        count = count + 1;
+    
+    }
+    average = sum/N;
+    printf("N = %d Sum = %f", N, sum);
+    printf("Average = %f", average);
+    return 0;
+}
